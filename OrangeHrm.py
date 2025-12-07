ba7d69d1175617fa6a26c79f62e21452689e8a5f
Akashdeep Chik Baraik <159066922+Akashdeep77-hx@users.noreply.github.com>
2025-12-07 23:12:35 +0530

    Add files via upload

---
+++ OrangeHrm.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+from trio import sleep
+
+# Start Chrome driver
+driver = webdriver.Chrome(service=Service("C:\\Windows\\chromedriver.exe"))
+
+# Open website
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+# Enter username
+driver.find_element(By.NAME, "username").send_keys("Admin")
+
+# Enter password
+driver.find_element(By.NAME, "password").send_keys("admin123")
+
+# Click Login button
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+driver.close()
