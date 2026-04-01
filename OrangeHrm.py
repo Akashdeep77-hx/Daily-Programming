e0e4a6d544a0c07b9a734858a796f70f94706565
Akashdeep Chik Baraik <159066922+Akashdeep77-hx@users.noreply.github.com>
2026-04-01 21:10:16 +0530

    Update OrangeHrm.py

--- OrangeHrm.py
+++ OrangeHrm.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-import time sleep sleep sleep
+import time sleep sleep sleep sleep
+
 
 
 
